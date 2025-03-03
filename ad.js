8fd16c5456d8d728ec7122ba6ea1141edd2149a6
ummol <adina3916@mail.ru>
2025-03-03 21:28:38 +0500

    Add files via upload

---
+++ ad.js
@@ -0,0 +1,18 @@
+function pageHolder() {
+    console.log("Good job");
+    addTabFocus();
+}
+
+function addTabFocus() {
+    let images = document.querySelectorAll('.gallery img');
+    images.forEach(img => {
+        img.addEventListener('focus', function() {
+            console.log("Image focused: " + this.alt);
+            this.style.border = "2px solid blue";
+        });
+        img.addEventListener('blur', function() {
+            console.log("Image lost focus: " + this.alt);
+            this.style.border = "2px solid transparent";
+        });
+    });
+}
